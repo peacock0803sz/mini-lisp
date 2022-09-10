1bcc17a5bc8cb7d58e8efa8fb8780c89e9ec6efd
Yoichi TAKAI <contact@peacock0803sz.com>
2022-09-10 18:27:51 +0900

    :white_check_mark: Add test cases

--- python/tests/test_lexer.py
+++ python/tests/test_lexer.py
@@ -5,7 +5,10 @@ from lisp_py.tokens import Tokens
 
 testdata = [
     ("1 + 3", ("1", Tokens.PLUS, "3")),
+    ("9 - 5", ("9", Tokens.MINUS, "5")),
+    ("4 * 8", ("4", Tokens.TIMES, "8")),
     ("2 / 3", ("2", Tokens.DIV, "3")),
+    ("10 undef 42", ("10", "undef", "42"))
 ]
 
 
